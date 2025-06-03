caadab568be71e5b75986d3eed2bbba7a34f67ce
Salomi Afolabi Onome <106906943+nomeo-310@users.noreply.github.com>
2025-06-03 21:33:54 +0100

    new changes made

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -32,3 +32,47 @@ export interface userDetailsProps {
   nationality: string;
   profile_picture: string;
 };
+
+export interface coinProps {
+  coin: string;
+  coin_name: string;
+  id: number;
+  method: string,
+  status: string;
+  icon: string;
+  stable_coins: string | null;
+};
+
+type coinType = {
+  id: number,
+  coin_name: string;
+  shorthand: string;
+  buy: string;
+  sell: string;
+  escrow: string;
+  status: string;
+  stable_coins: string;
+  created_at: string;
+  updated_at: string;
+};
+
+type limitType = {
+  buying_limit: string;
+  selling_limit: string;
+  card_limit: string;
+  data_limit: string;
+  card_limit_active: number;
+  data_limit_active: number;
+};
+
+export interface minTransaction {
+  status: string;
+  message: string;
+  coin: coinType;
+  limit: limitType;
+  current_rate: number;
+  transaction_charges: number;
+  sell_naira_value: string;
+  buy_naira_value: string;
+  icon: string;
+};
